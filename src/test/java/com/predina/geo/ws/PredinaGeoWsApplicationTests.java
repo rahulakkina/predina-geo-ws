e899c7b7d94748f6c3e42a9e00d7e9f88e54cac6
Rahul Anand Akkina <rahul.akkina@gmail.com>
2018-07-20 09:55:54 +0800

    Changed to http for simplicity

--- src/test/java/com/predina/geo/ws/PredinaGeoWsApplicationTests.java
+++ src/test/java/com/predina/geo/ws/PredinaGeoWsApplicationTests.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 //@SpringBootTest
 public class PredinaGeoWsApplicationTests {
 
-	//@Test
+	@Test
 	public void contextLoads() {
+		System.out.println("test");
 	}
 
 }
