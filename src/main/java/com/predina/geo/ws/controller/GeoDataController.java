c29199d69366287b24e7ca0e0f49967b49bf8254
anrfgd <rahul.anandakkina@bp.com>
2018-07-24 12:48:04 +0800

    Changes in the endpoint

--- src/main/java/com/predina/geo/ws/controller/GeoDataController.java
+++ src/main/java/com/predina/geo/ws/controller/GeoDataController.java
@@ -43,20 +43,20 @@ public class GeoDataController {
 
     @CrossOrigin(origins = "*")
     @GetMapping("/all")
-    public ResponseEntity<List<Map<String, Serializable>>> all() {
-        return new ResponseEntity<List<Map<String, Serializable>>>(
-                Lists.transform(geoDataService.findAll(),this::transform), HttpStatus.OK);
+    public ResponseEntity<List<List<Serializable>>> all() {
+        return new ResponseEntity<List<List<Serializable>>>(
+                Lists.transform(geoDataService.findAll(),this::transformList), HttpStatus.OK);
     }
 
 
     @CrossOrigin(origins = "*")
-    @GetMapping("/page/{pageId}")
-    public ResponseEntity<Map<String, Serializable>> page(@PathVariable("pageId") final Integer pageId){
+    @GetMapping("/page/{pageId}/{pageSize}")
+    public ResponseEntity<Map<String, Serializable>> page(@PathVariable("pageId") final Integer pageId, @PathVariable("pageSize") final Integer pageSize){
         final Map<String, Serializable> result = Maps.newHashMapWithExpectedSize(3);
-        final List<List<GeoMapLocation>> pages = Lists.partition(geoDataService.findAll(), 22850);
+        final List<List<GeoMapLocation>> pages = Lists.partition(geoDataService.findAll(), pageSize);
         if(!CollectionUtils.isEmpty(pages)) {
-            final ImmutableList<Map<String, Serializable>> data = ImmutableList.copyOf((pageId != null && pageId <= pages.size()) ?
-                              Lists.transform(pages.get(pageId - 1),this::transform) : Lists.transform(pages.get(0),this::transform));
+            final ImmutableList<List<Serializable>> data = ImmutableList.copyOf((pageId != null && pageId <= pages.size()) ?
+                              Lists.transform(pages.get(pageId - 1),this::transformList) : Lists.transform(pages.get(0),this::transformList));
             result.put("d", data);
             result.put( "p", (pageId != null && pageId <= pages.size()) ? pageId : 1);
             result.put("m", pages.size());
@@ -70,15 +70,15 @@ public class GeoDataController {
 
     @CrossOrigin(origins = "*")
     @GetMapping("/lookup/{lat}/{lng}")
-    public ResponseEntity<Map<String, Serializable>> lookup(@PathVariable("lat") final Double lat, @PathVariable("lng") final Double lng) {
-        return new ResponseEntity<Map<String, Serializable>>(transform(getGeoDataService().lookup(new GeoCoordinate(lat, lng))), HttpStatus.OK);
+    public ResponseEntity<List<Serializable>> lookup(@PathVariable("lat") final Double lat, @PathVariable("lng") final Double lng) {
+        return new ResponseEntity<List<Serializable>>(transformList(getGeoDataService().lookup(new GeoCoordinate(lat, lng))), HttpStatus.OK);
     }
 
     @CrossOrigin(origins = "*")
     @GetMapping("/update/{lat}/{lng}/{riskScore}")
     @SendTo("/topic/geoDataUpdates")
-    public ResponseEntity<Map<String, Serializable>> update(@PathVariable("lat") final Double lat, @PathVariable("lng") final Double lng, @PathVariable("riskScore") final Integer riskScore) {
-        return new ResponseEntity<Map<String, Serializable>>(transform(getGeoDataService().update(new GeoCoordinate(lat, lng), riskScore)), HttpStatus.OK);
+    public ResponseEntity<List<Serializable>> update(@PathVariable("lat") final Double lat, @PathVariable("lng") final Double lng, @PathVariable("riskScore") final Integer riskScore) {
+        return new ResponseEntity<List<Serializable>>(transformList(getGeoDataService().update(new GeoCoordinate(lat, lng), riskScore)), HttpStatus.OK);
     }
 
 
@@ -87,6 +87,10 @@ public class GeoDataController {
                 "lng",geoMapLocation.getCoords().getLng(),
                 "rs", geoMapLocation.getRs(),
                 "cin",geoMapLocation.getGid());
+    }
+
+    protected List<Serializable> transformList(final GeoMapLocation geoMapLocation){
+        return ImmutableList.of(geoMapLocation.getCoords().getLat(),geoMapLocation.getCoords().getLng(),geoMapLocation.getGid());
     }
 
 
